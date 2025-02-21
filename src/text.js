a7a8a8d92cd56c7bd5b557f0b84920d6587acaf5
ramon <ramon@cherryline.ru>
2025-02-21 14:31:33 +0300

    header start animation

--- src/text.js
+++ src/text.js
@@ -1,18 +1,40 @@
 import { gsap} from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin"
 
 gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin( MotionPathPlugin );
 
+const path = [
+    { x: 800, y: 0 },
+    // { x: 20, y: 20 },
+
+];
 
 const checkForElements = setInterval(() => {
     const elements = document.querySelectorAll('#header');
     if (elements.length > 0) {
-        gsap.to("#header", {
-            duration: 1, // Длительность анимации
-            text: "HEADER", // Новый текст
-            ease: "elastic.inOut" // Эффект плавности
-        });
-        clearInterval(checkForElements); // Останавливаем проверку
+       animations()
+        clearInterval(checkForElements); 
     }
 }, 100);
 
+
+function animations(){
+
+    gsap.to(".header-text", {
+        duration: 1,
+        text: "HEADER",
+        ease: "expo.inOut" ,
+        yoyo: true,
+        onComplete: () => {
+            gsap.to(".header-text", {
+                motionPath: {
+                    path: path,
+                },
+                duration: 1,
+                delay: 0.2
+            });
+        }
+    })
+}
